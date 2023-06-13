c5d51cddd2214c4ab34a0eaacb111ecaffc57960
gauravkumarjec <125721895+gauravkumarjec@users.noreply.github.com>
2023-06-13 16:05:03 +0530

    add initial change

---
+++ index.js
@@ -0,0 +1,84 @@
+
+// button is detecting
+
+var numberOfdrum=document.querySelectorAll(".drum").length;
+
+for(var i=0;i<numberOfdrum;i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click" , function(){ 
+          
+               var buttonPress=this.innerHTML;
+               makeSound(buttonPress);
+               buttonAnninmation(buttonPress);
+
+
+        
+
+});
+}
+
+// keyboard key detecting
+
+document.addEventListener("keypress",function(event){
+    
+    makeSound(event.key);
+    buttonAnninmation(event.key);       // key keyword tells which key had pressed
+});
+
+function makeSound(key){
+        
+    switch (key) {
+        case "w":
+            var tom1=new Audio('sounds/tom-1.mp3');
+            tom1.play();         
+            break;
+
+        case "a":
+            var tom2=new Audio('sounds/tom-2.mp3');
+            tom2.play(); 
+            break;
+
+        case "s":
+            var tom3=new Audio('sounds/tom-3.mp3');
+            tom3.play(); 
+            break;     
+        
+        case "d":
+            var tom4=new Audio('sounds/tom-4.mp3');
+            tom4.play(); 
+            break; 
+            
+        case "j":
+            var snare=new Audio('sounds/snare.mp3');
+            snare.play(); 
+            break;
+
+        case "k":
+            var crash=new Audio('sounds/crash.mp3');
+            crash.play(); 
+            break; 
+            
+        case "l":
+            var kick=new Audio('sounds/kick-bass.mp3');
+            kick.play(); 
+            break;    
+
+        default:
+            console.log(key);
+            break;
+    }
+}
+
+function buttonAnninmation(currentkey){
+    
+    var activeButton=document.querySelector("."+ currentkey);
+
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");   //tells after how much time in millisecond class pressed will remove
+    },100);
+}
+
+
+
